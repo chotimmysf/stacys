065a7e6d1213a505f9835d3c60f81f291ec1b1bb
Timmy Cho <chotimmysf@gmail.com>
2022-06-28 19:22:15 -0700

    router link and navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return (
+    <p>Stacy's Online Shop</p>
+  )
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={
-        <Home />
-      } />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
