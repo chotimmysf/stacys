6cfe5eb88fd10d52b330dcca6b6ed8a72f4bd090
Timmy Cho <chotimmysf@gmail.com>
2022-07-15 16:58:15 -0700

    sign in with Google redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,9 @@
-import { signInWithGooglePopUp, createUserDocumentFromAuth, } from "../../utilities/firebase/firebase.utilities";
+import { 
+    auth,
+    signInWithGooglePopUp, 
+    signInWithGoogleRedirect, 
+    createUserDocumentFromAuth, 
+} from "../../utilities/firebase/firebase.utilities";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
@@ -11,7 +16,8 @@ const SignIn = () => {
             <h1>Sign In to see your Stacy's Stars Rewards!</h1>
             <p>Having a Stars Rewards account with Stacy's allows you to earn 2% unlimited cash back on all purchases made on stacys.com and Stacy's stores!</p>
             <p>To accomodate for the needs of all our customers, we have multiple ways for you to sign in and view your Stars Rewards!</p>
-            <button onClick={logGoogleUser}>Sign In via Google Popup</button>
+            <button onClick={logGoogleUser}>Sign In via Google (Popup)</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In via Google (Redirect)</button>
         </div>
     );
 };
