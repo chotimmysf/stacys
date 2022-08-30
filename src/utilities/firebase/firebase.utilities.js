8912bdb38291043e43d9144d92472b23c171efaa
Timmy Cho <chotimmysf@gmail.com>
2022-08-29 18:48:09 -0700

    adding collection & docs to fetch data from Firebase

--- src/utilities/firebase/firebase.utilities.js
+++ src/utilities/firebase/firebase.utilities.js
@@ -8,7 +8,7 @@ import { getAuth,
     signOut,
     onAuthStateChanged
  } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -35,6 +35,33 @@ const firebaseConfig = {
   export const signInWithGooglePopUp = () => signInWithPopup(auth, googleProvider);
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+        const docRef= doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+  };
+
+  export const getCategoriesAndDocuments = async() => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+  }
 
   export const createUserDocumentFromAuth = async(userAuth) => {
       if(!userAuth) return;
