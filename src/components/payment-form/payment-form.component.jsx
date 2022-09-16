cd009cd38b4f3198e573a76c53abf40d50cb292c
Timmy Cho <chotimmysf@gmail.com>
2022-09-16 11:55:20 -0700

    Netlify Integration

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -12,11 +12,21 @@ const PaymentForm = () => {
         if(!stripe || !elements) {
             return;
         }
-    }
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+
+        console.log(response);
+    };
 
     return (
         <div className='payment-form-container'>
-            <form>
+            <form onSubmit={paymentHandler}>
                 <h1>Payment Information</h1>
                 <CardElement/>
                 <Button buttonType={BUTTON_TYPES_CLASSES.inverted}>Purchase</Button>
